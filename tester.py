af4af93fdfe2c8c92a192d2f0e5f75f4072301e8
zmz0305 <zmz305@gmail.com>
2017-01-06 20:41:40 +0800

    style improvement

--- tester.py
+++ tester.py
@@ -7,8 +7,9 @@ parser.add_argument("-s", "--seed", type=int, default=0,
 					help='the seed for random generator')
 parser.add_argument("-m", "--mode", default='FULL', 
 					help='the NonDex mode')
-parser.add_argument("-b", "--bin", default="/Users/zmz0305/workspace/OneDrive/workplace/research/install-python/bin/python3.7",
-					help="the modified python executable path")
+parser.add_argument("-b", "--bin", default="/Users/zmz0305/workspace/\
+				OneDrive/workplace/research/install-python/bin/python3.7",
+				help="the modified python executable path")
 args = parser.parse_args()
 os.environ['PY_SRAND'] = str(args.seed)
 os.environ['NONDEX_MODE'] = args.mode
